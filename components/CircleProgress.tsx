446cc35e0e11f0b4b6a002a52182224f0c0a2b63
Adham ElGazoly <elgazoly11@gmail.com>
2023-04-10 20:13:51 +0200

    mobile page header added timer and some customized colors for the theme

---
+++ components/CircleProgress.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
+
+const Timer = ({}) => {
+  const [time, setTime] = useState(180) // 3 minutes in seconds
+  const [isActive, setIsActive] = useState(false)
+
+  const toggleTimer = () => {
+    setIsActive(!isActive)
+  }
+
+  const resetTimer = () => {
+    setIsActive(false)
+    setTime(180)
+  }
+
+  const formatTime = (seconds: number) => {
+    const minutes = Math.floor(seconds / 60)
+    const remainingSeconds = seconds % 60
+    return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`
+  }
+
+  const circleProps = {
+    strokeDasharray: '565.48',
+    strokeDashoffset: ((565.48 * (180 - time)) / 180).toString(),
+    strokeWidth: '6',
+    stroke: isActive ? 'var(--timer-red)' : 'var(--timer-gray)',
+  }
+
+  return (
+    <CountdownCircleTimer
+    isPlaying = {isActive}
+    trailColor={'#f06292'}
+    size={123}
+    duration={25*60}
+    strokeWidth={25}
+    colors={['#03045E', '#F7B801', '#A30000', '#A30000']}
+    colorsTime={[7, 5, 2, 0]}
+  >
+    {({ remainingTime }) => 
+    <div className=' text-3xl font-extrabold flex flex-col gap-y-3'>
+    {formatTime(remainingTime)}
+    <span>Focus</span>
+    </div>
+    }
+  </CountdownCircleTimer>
+  )
+}
+
+export default Timer
