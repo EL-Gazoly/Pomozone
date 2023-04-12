ebe45ed3adf5575babbde6122271a39538feddc0
Adham ElGazoly <elgazoly11@gmail.com>
2023-04-12 22:29:45 +0200

    Update class names for signin-button and logo to align with responsive breakpoints

--- components/rightnav.tsx
+++ components/rightnav.tsx
@@ -5,13 +5,14 @@ const RightNav = () => {
 
         <>
          <div className="right hidden md:grid grid-cols-1 w-64 h-screen border bg-white border-border items-center justify-center   ">
-      <div className="logo flex flex-row items-center justify-center mr-6 mt-16 self-start">
+          <div className="self-start grid grid-cols-1 gap-y-16">
+      <div className="logo flex flex-row items-center justify-center mr-6 mt-16 ">
             <Image src="/logo.svg" alt="logo"  width={58}  height={46}   quality={100}/>
             <span className={` font-extrabold text-xl text-primary` }> Pomozone</span>
           </div>
 
       
-    <div className="div grid grid-cols-1  flex-1 items-center justify-center gap-y-20 ml-5 ">
+    <div className="div grid grid-cols-1  flex-1 items-center justify-center  gap-y-20 ml-5 ">
       
       <Link href="/" className="time flex flex-row items-center  gap-x-2 cursor-pointer ml-4">
                   <Image src="/timeIcon.svg" width={37} height={27} alt="time" quality={100}/>
@@ -25,6 +26,8 @@ const RightNav = () => {
 
               
         
+
+      </div>
 
       </div>
       <div className="lightMode flex flex-row items-center ml-10 gap-x-2 opacity-70 hover:opacity-100 self-end mb-14 cursor-pointer">
