bfd78aa857280ba0ebf0a055cf254b115b394c83
Adham ElGazoly <elgazoly11@gmail.com>
2023-04-13 22:28:17 +0200

    Add useTheme hook and update Footer to respect system and theme themes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter } from 'next/font/google'
 import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,15 +12,16 @@ const Footer = () => {
     }, []);
     
       const [DarkMode, setDarkMode] = useState(false);
+      const { systemTheme, theme, setTheme } = useTheme();
+
+        const currentTheme = theme === "system" ? systemTheme : theme;
 
     return ( 
         <footer className="footer grid grid-col text-center gap-y-4 absolute bottom-20 md:ml-64">
-            <span className={`${inter.className} font-medium text-sm ml-3
-            ${DarkMode ? 'text-white' : ''}
-            `} >Crafted with passion by Adham ElGazoly</span>
+            <span className={`${inter.className} font-medium text-sm ml-3 `} >Crafted with passion by Adham ElGazoly</span>
             <div className="social flex flex-row gap-x-3 justify-self-center">
                 <a href="https://www.linkedin.com/in/adham-el-gazoly-075385190/" target="_blank">
-                    { DarkMode ? (
+                    { currentTheme === "dark" ? (
                     <Image src="/dark-linkedin.svg" width={24} height={24} alt="linkedin" quality={100}/>
                     ) : (
                     <Image src="/linkedin.svg" width={24} height={24} alt="linkedin" quality={100}/>
@@ -27,7 +29,7 @@ const Footer = () => {
                     </a>
 
                 <a href="https://github.com/EL-Gazoly" target="_blank">
-                    { DarkMode ? (
+                    { currentTheme === "dark" ? (
                     <Image src="/dark-github.svg" width={24} height={24} alt="github" quality={100}/>
                     ) : (
                     <Image src="/github.svg" width={24} height={24} alt="github" quality={100}/>
